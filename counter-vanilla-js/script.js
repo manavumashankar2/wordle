3d5bfb20192a15a14dba15df43875193bcb0be54
Manav K <78536184+manavumashankar2@users.noreply.github.com>
2022-07-15 13:57:38 +0530

    Delete counter-vanilla-js directory

--- counter-vanilla-js/script.js
+++
@@ -1,6 +0,0 @@
-const incrementButton = document.getElementById("increment")
-incrementButton.addEventListener("click", () => {
-  const counter = document.getElementById("counter")
-  const next = Number(counter.innerText) + 1
-  counter.innerText = next.toString()
-})
